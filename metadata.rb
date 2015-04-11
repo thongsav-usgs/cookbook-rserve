81bf8618cf11686d9cda69d3cb99fee9d0019e75
Suftin, Ivan <isuftin@usgs.gov>
2015-04-10 22:18:25 -0500

    Upped version, ready to tag

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'Public Domain'
 description      'Installs/Configures RServe'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'yum'
 depends 'iptables'
